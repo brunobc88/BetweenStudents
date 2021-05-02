ab71b7e20222040d3fd0d60b3f4d48e18fe0516f
brunobc88 <73168161+brunobc88@users.noreply.github.com>
2021-05-02 12:51:51 +0200

    Update Entity Campus

---
+++ src/Entity/Campus.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CampusRepository::class)
+ */
+class Campus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Sortie::class, mappedBy="campus")
+     */
+    private $sorties;
+
+    /**
+     * @ORM\OneToMany(targetEntity=User::class, mappedBy="campus")
+     */
+    private $users;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Ville::class, inversedBy="campus")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $ville;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public function __construct()
+    {
+        $this->sorties = new ArrayCollection();
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Sortie[]
+     */
+    public function getSorties(): Collection
+    {
+        return $this->sorties;
+    }
+
+    public function addSortie(Sortie $sorties): self
+    {
+        if (!$this->sorties->contains($sorties)) {
+            $this->sorties[] = $sorties;
+            $sorties->setCampus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSortie(Sortie $sorties): self
+    {
+        if ($this->sorties->removeElement($sorties)) {
+            // set the owning side to null (unless already changed)
+            if ($sorties->getCampus() === $this) {
+                $sorties->setCampus(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setCampus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getCampus() === $this) {
+                $user->setCampus(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getVille(): ?Ville
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?Ville $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+}
